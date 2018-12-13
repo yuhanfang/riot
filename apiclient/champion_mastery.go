6b2fb02d04353a119ec7791383c4feaa2932ee64
Tyler Hill <Tyler@Abstract.kr>
2018-12-13 10:21:21 -0600

    Convert v3 endpoints to v4
    Should be everything, however I'll still go through and update comments
    on interfaces/structs.
    
    Signed-off-by: Tyler Hill <Tyler@Abstract.kr>

--- apiclient/champion_mastery.go
+++ apiclient/champion_mastery.go
@@ -13,26 +13,26 @@ type ChampionMastery struct {
 	ChampionLevel                int               `datastore:",noindex"` // Champion level for specified player and champion combination.
 	ChampionPoints               int               `datastore:",noindex"` // Total number of champion points for this player and champion combination - they are used to determine championLevel.
 	ChampionID                   champion.Champion `datastore:",noindex"` // Champion ID for this entry.
-	PlayerID                     int64             `datastore:",noindex"` // Player ID for this entry.
+	PlayerID                     string            `datastore:",noindex"` // Encrypted Player ID for this entry.
 	ChampionPointsUntilNextLevel int64             `datastore:",noindex"` // Number of points needed to achieve next level. Zero if player reached maximum champion level for this champion.
 	ChampionPointsSinceLastLevel int64             `datastore:",noindex"` // Number of points earned since current level has been achieved. Zero if player reached maximum champion level for this champion.
 	LastPlayTime                 int64             `datastore:",noindex"` // Last time this champion was played by this player - in Unix milliseconds time format.
 }
 
-func (c *client) GetAllChampionMasteries(ctx context.Context, r region.Region, summonerID int64) ([]ChampionMastery, error) {
+func (c *client) GetAllChampionMasteries(ctx context.Context, r region.Region, summonerID string) ([]ChampionMastery, error) {
 	var res []ChampionMastery
-	_, err := c.dispatchAndUnmarshal(ctx, r, "/lol/champion-mastery/v3/champion-masteries/by-summoner", fmt.Sprintf("/%d", summonerID), nil, &res)
+	_, err := c.dispatchAndUnmarshal(ctx, r, "/lol/champion-mastery/v4/champion-masteries/by-summoner", fmt.Sprintf("/%s", summonerID), nil, &res)
 	return res, err
 }
 
-func (c *client) GetChampionMastery(ctx context.Context, r region.Region, summonerID int64, champ champion.Champion) (*ChampionMastery, error) {
+func (c *client) GetChampionMastery(ctx context.Context, r region.Region, summonerID string, champ champion.Champion) (*ChampionMastery, error) {
 	var res ChampionMastery
-	_, err := c.dispatchAndUnmarshalWithUniquifier(ctx, r, "/lol/champion-mastery/v3/champion-masteries/by-summoner", fmt.Sprintf("/%d/by-champion/%d", summonerID, champ), nil, "by-champion", &res)
+	_, err := c.dispatchAndUnmarshalWithUniquifier(ctx, r, "/lol/champion-mastery/v4/champion-masteries/by-summoner", fmt.Sprintf("/%s/by-champion/%d", summonerID, champ), nil, "by-champion", &res)
 	return &res, err
 }
 
-func (c *client) GetChampionMasteryScore(ctx context.Context, r region.Region, summonerID int64) (int, error) {
+func (c *client) GetChampionMasteryScore(ctx context.Context, r region.Region, summonerID string) (int, error) {
 	var res int
-	_, err := c.dispatchAndUnmarshal(ctx, r, "/lol/champion-mastery/v3/scores/by-summoner", fmt.Sprintf("/%d", summonerID), nil, &res)
+	_, err := c.dispatchAndUnmarshal(ctx, r, "/lol/champion-mastery/v4/scores/by-summoner", fmt.Sprintf("/%s", summonerID), nil, &res)
 	return res, err
 }
