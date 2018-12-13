1fd518ef9758958c9157f337044c146a84be67e2
Tyler Hill <Tyler@Abstract.kr>
2018-12-13 10:30:04 -0600

    Missed third_party
    Signed-off-by: Tyler Hill <Tyler@Abstract.kr>

--- apiclient/third_party.go
+++ apiclient/third_party.go
@@ -7,8 +7,8 @@ import (
 	"github.com/yuhanfang/riot/constants/region"
 )
 
-func (c *client) GetThirdPartyCodeByID(ctx context.Context, r region.Region, summonerID int64) (string, error) {
+func (c *client) GetThirdPartyCodeByID(ctx context.Context, r region.Region, summonerID string) (string, error) {
 	var res string
-	_, err := c.dispatchAndUnmarshal(ctx, r, "/lol/platform/v3/third-party-code/by-summoner", fmt.Sprintf("/%d", summonerID), nil, &res)
+	_, err := c.dispatchAndUnmarshal(ctx, r, "/lol/platform/v4/third-party-code/by-summoner", fmt.Sprintf("/%s", summonerID), nil, &res)
 	return res, err
 }
